4865a8b68fbc4919dd21dbe15e2baef4c18e45c9
Nikita Klimuk <nikitaklimukwot@gmail.com>
2023-05-13 13:45:14 +0200

    works with router and create mainPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
-import React from "react";
+import { Routes, Route } from "react-router-dom";
+import MainPage from "./pages/MainPage";
 import "./styles/App.scss";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <section className="App">
+      {/* <Navbar /> */}
+      <main className="main">
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          {/* <Route path="/:id" element={<DetailsPage />} /> */}
+        </Routes>
+      </main>
+      {/* <Footer /> */}
+    </section>
+  );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,13 @@
-import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
-import "./styles/index.scss";
 import App from "./App";
+import "./styles/index.scss";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(<App />);
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
